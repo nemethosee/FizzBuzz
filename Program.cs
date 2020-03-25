53b2e9f6d111e72f315275783ee505705ef8aaae
Oszkar Nemeth <oszkar.nemeth@outlook.com>
2020-03-25 18:57:50 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+
+    public class cls_FizzBuzz
+    {
+        public String FizzBuzz(int InputNumber)
+        {
+            return null;
+        }
+    }
+}
