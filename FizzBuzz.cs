a642eacf5b7ad61fb364a16c37ff2f7a9e53d33d
Oszkar Nemeth <oszkar.nemeth@outlook.com>
2020-03-25 19:03:21 +0200

    Files renamed.

--- FizzBuzz.cs
+++ FizzBuzz.cs
@@ -6,14 +6,6 @@ using System.Threading.Tasks;
 
 namespace FizzBuzz
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-        }
-    }
-
-
     public class cls_FizzBuzz
     {
         public String FizzBuzz(int InputNumber)
